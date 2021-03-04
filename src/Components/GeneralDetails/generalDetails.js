27ef264f40d07843a686efb93c5a21e1abb89479
nipa85 <nikos.pap85@gmail.com>
2021-03-04 18:46:09 +0200

    general details balances

--- src/Components/GeneralDetails/generalDetails.js
+++ src/Components/GeneralDetails/generalDetails.js
@@ -7,26 +7,32 @@ import "./style.css"
 const GeneralDetails = (props) => {
     return (
         <Section>
-            <GeneralDetailsItem>
-                <GeneralDetailsItemTitle  title="Market Overview"/>
-                    <GeneralDetailsItemContent>
-                        <GeneralDetailsItemContentItem label="Total Supply:" 
-                            value={`$${convertToLargeNumberRepresentation(props.generalData?.allMarketsTotalSupplyBalance?.precision(4))}`}/>
-                        <GeneralDetailsItemContentItem label="Total Borrow:" 
-                            value={`$${convertToLargeNumberRepresentation(props.generalData?.allMarketsTotalBorrowBalance?.precision(4))}`}/>
-                        <GeneralDetailsItemContentItem label="Total Liquidity:" 
-                            value={`$${convertToLargeNumberRepresentation(props.generalData?.totalLiquidity?.precision(4))}`}/>
-                    </GeneralDetailsItemContent>
+                <GeneralDetailsItem>
+                    <GeneralDetailsItemTitle  title="Market Overview"/>
+                        <GeneralDetailsItemContent>
+                            <GeneralDetailsItemContentItem label="Total Supply:" 
+                                value={`$${convertToLargeNumberRepresentation(props.generalData?.allMarketsTotalSupplyBalance?.precision(4))}`}/>
+                            <GeneralDetailsItemContentItem label="Total Borrow:" 
+                                value={`$${convertToLargeNumberRepresentation(props.generalData?.allMarketsTotalBorrowBalance?.precision(4))}`}/>
+                            <GeneralDetailsItemContentItem label="Total Liquidity:" 
+                                value={`$${convertToLargeNumberRepresentation(props.generalData?.totalLiquidity?.precision(4))}`}/>
+                        </GeneralDetailsItemContent>
                 </GeneralDetailsItem>
                 <GeneralDetailsItem>
-                    <GeneralDetailsItemTitle  title="Your Supply Balance"/>
-                    <GeneralDetailsItemContent>
-                        <GeneralDetailsItemContentItem className="center"
-                            value={`$${zeroStringIfNullish(props.generalData?.totalSupplyBalance?.toFixed(2), 2)}`}/>
-                    </GeneralDetailsItemContent>
+                    <GeneralDetailsItemTitle  title="Balances"/>
+                        <GeneralDetailsItemContent>
+                            <GeneralDetailsItemContentItem label="Supply:" 
+                                value={`$${zeroStringIfNullish(props.generalData?.totalSupplyBalance?.toFixed(2), 2)}`}/>
+                            <GeneralDetailsItemContentItem label="Borrow:" 
+                                value={`$${zeroStringIfNullish(props.generalDetails?.totalBorrowBalance?.toFixed(2),2)}`}/>
+                            <GeneralDetailsItemContentItem label="Borrow Limit:" 
+                                value={`$${zeroStringIfNullish(props.generalDetails?.totalBorrowLimit?.toFixed(2),2)}`}
+                                valueDetails={`(${zeroStringIfNullish(props.generalDetails?.totalBorrowLimitUsedPercent?.toFixed(2),2)}% Used)`}/>
+                        </GeneralDetailsItemContent>
                 </GeneralDetailsItem>
+                
                 <GeneralDetailsItem>
-                    <GeneralDetailsItemTitle  title="Net Apy"/>
+                    <GeneralDetailsItemTitle  title="Net APY"/>
                     <GeneralDetailsItemContent>
                         <GeneralDetailsItemContentItem className="center"
                             value={`${zeroStringIfNullish(props.generalDetails?.netApy?.times(100).toFixed(2),2)}%`}/>
@@ -36,21 +42,7 @@ const GeneralDetails = (props) => {
                             value={`+ ${zeroStringIfNullish(props.generalDetails?.totalBorrowPctApy?.times(100).toFixed(2),2)}%`}/>
                     </GeneralDetailsItemContent>
                 </GeneralDetailsItem>
-                <GeneralDetailsItem>
-                <GeneralDetailsItemTitle  title="Your Borrow Balance"/>
-                    <GeneralDetailsItemContent>
-                        <GeneralDetailsItemContentItem className="center"
-                            value={`$${zeroStringIfNullish(props.generalDetails?.totalBorrowBalance?.toFixed(2),2)}`}/>
-                    </GeneralDetailsItemContent>
-                </GeneralDetailsItem>
-                <GeneralDetailsItem>
-                    <GeneralDetailsItemTitle  title="Your Borrow Limit"/>
-                    <GeneralDetailsItemContent>
-                        <GeneralDetailsItemContentItem className="center"
-                            value={`$${zeroStringIfNullish(props.generalDetails?.totalBorrowLimit?.toFixed(2),2)}`}
-                            valueDetails={`(${zeroStringIfNullish(props.generalDetails?.totalBorrowLimitUsedPercent?.toFixed(2),2)}% Used)`}/>
-                    </GeneralDetailsItemContent>
-                </GeneralDetailsItem>
+                
                     
         </Section>
     )
