198f8d7a882363e7baf12b64f709134ac0f0ee14
nipa85 <nikos.pap85@gmail.com>
2021-03-08 18:10:12 +0200

    spinner fixes

--- src/assets/huIcons/huIcons.js
+++ src/assets/huIcons/huIcons.js
@@ -200,8 +200,8 @@ const Spinner = (props) => {
         height: props.size ? props.size : "50px"
     }
     return(
-        <svg style={style} class="spinner" viewBox="0 0 50 50">
-            <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        <svg style={style} className="spinner" viewBox="0 0 50 50">
+            <circle className="path" cx="25" cy="25" r="20" fill="none"></circle>
         </svg>
     )
 }
