fcd189064e50d1e2d6df4e1d5308e28c93667eaf
nipa85 <nikos.pap85@gmail.com>
2021-06-22 22:33:22 +0300

    test network enviroment variable

--- src/networks.tsx
+++ src/networks.tsx
@@ -18,7 +18,7 @@ type NetworkData = {
     [key:string]: Network
 }
 
-const NETWORKS: NetworkData = {
+const NETWORKS: NetworkData = !process.env.REACT_APP_TEST_NETWORK ? {
     "0x1" : {
         chainId: "0x1",
         network: "Ethereum Mainnet",
@@ -87,6 +87,21 @@ const NETWORKS: NetworkData = {
         HUNDRED_ADDRESS: "0xfCD8570AD81e6c77b8D252bEbEBA62ed980BD64D",
         COMPOUND_LENS_ADDRESS: "0xd513d22422a3062Bd342Ae374b4b9c20E0a9a074",
     }       
+} : {
+    "0x2a":
+    {
+        chainId: "0x2a",
+        network: "KOVAN Test Network",
+        symbol: "ETH",
+        rpcUrls: ["https://kovan.infura.io/v3/undefined"],
+        blockExplorelUrls: ["https://kovan.etherscan.io"],
+        logo: Logos["ETH"],
+        name: "ETH",
+        token: "0xfcd8570ad81e6c77b8d252bebeba62ed980bd64d",
+        UNITROLLER_ADDRESS: "0x0f390559f258eb8591c8e31cf0905e97cf36ace2",
+        HUNDRED_ADDRESS: "0x10010054c81c8c01951E6A631dD228bB02d8D1e5",
+        COMPOUND_LENS_ADDRESS: "0xd513d22422a3062Bd342Ae374b4b9c20E0a9a074",
+    }
 }
 
 export { NETWORKS }
