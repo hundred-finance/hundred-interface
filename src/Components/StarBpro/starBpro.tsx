f14a11e374f2d897ff886fa7cce66553ce86e5bf
jbeanz <jbeanzsoup@gmail.com>
2022-01-27 16:38:24 -0500

    added tooltip for backstop logo - fix duplicate error

--- src/Components/StarBpro/starBpro.tsx
+++ src/Components/StarBpro/starBpro.tsx
@@ -1,30 +1,51 @@
-import React from "react"
-import "./starBpro.css"
-import star from "../../assets/icons/rating-star.svg"
-import bpro from "../../assets/images/BPRO-logo.svg"
+import React from 'react';
+import './starBpro.css';
+import star from '../../assets/icons/rating-star.svg';
+import bpro from '../../assets/images/BPRO-logo.svg';
+import ReactTooltip from 'react-tooltip';
+import { useEffect } from 'react';
 
 interface Props {
-    active: boolean,
-    backstop: boolean
+    active: boolean;
+    backstop: boolean;
 }
 
-const StarBpro: React.FC<Props> = (props: Props) =>{
-    return(
-        props.active && props.backstop ?
-            <div className="star-bpro star-bpro-icons">
-                <img src={bpro}/>
-                <img src={star}/>
-            </div>
-        : props.active ?
-            <div className="star star-bpro-icons">
-                <img src={star}/>
-            </div>
-        : props.backstop ?
-            <div className="bpro star-bpro-icons">
-                <img src={bpro}/>
-            </div>
-        : <></>
-    )
-}
+const StarBpro: React.FC<Props> = (props: Props) => {
+    useEffect(() => {
+        ReactTooltip.rebuild();
+    });
 
-export default StarBpro
+    return props.active && props.backstop ? (
+        <div className="star-bpro star-bpro-icons">
+            <img src={bpro} />
+            <img src={star} />
+        </div>
+    ) : props.active ? (
+        <div className="star star-bpro-icons">
+            <img src={star} />
+        </div>
+    ) : props.backstop ? (
+        <div className="bpro star-bpro-icons">
+            <img src={bpro} data-tip data-for="BPRO" />
+
+            <ReactTooltip id="BPRO" place="top" effect="solid" delayHide={100} delayShow={100} delayUpdate={100}>
+                <p>
+                    Green logo indicates markets integrated with a backstop provision.</p>
+                    <p>
+                    Learn about backstop{' '}
+                    <a
+                        className="a"
+                        href="https://docs.hundred.finance/core-protocol/backstop-provision"
+                    >
+                        here
+                    </a>{' '}
+                </p>
+            </ReactTooltip>
+            
+        </div>
+    ) : (
+        <></>
+    );
+};
+
+export default StarBpro;
